a62cdf0ed019c17078e3bb3a6e7dffe5ae29f161
Nithin Pranesh <nithinpra7@gmail.com>
2022-05-21 00:32:04 -0400

    remove some unneeded files

--- compile_shaders.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.3.211.0\Bin\glslc.exe ./Shaders/SimpleTriangle.vert -o ./Shaders/Compiled/vert.spv
-C:\VulkanSDK\1.3.211.0\Bin\glslc.exe ./Shaders/SimpleTriangle.frag -o ./Shaders/Compiled/frag.spv
-pause
