d5a4a9c9307f59207056436891f04818c8ae4ec7
Nithin Pranesh <nithinpra7@gmail.com>
2022-12-22 12:11:49 -0500

    use feedback controller to smooth keyboard inputs

--- Include/CameraController.h
+++ Include/CameraController.h
@@ -3,6 +3,8 @@
 #include "Camera.h"
 #include "InputManager.h"
 
+#include <glm/glm.hpp>
+
 namespace AltheaEngine {
 class CameraController {
 public:
@@ -16,18 +18,20 @@ public:
   }
 
 private:
-  // Camera movement callbacks:
-  void _moveForward();
-  void _moveBackward();
-  void _strafeLeft();
-  void _strafeRight();
-  void _ascend();
-  void _descend();
-
   // Mouse updates, using normalized screen coordinates.
   void _updateMouse(float x, float y);
 
-  float _movementSpeed = 0.1f;
+  void _updateTargetDirection(uint32_t axis, int dir);
+
+  // The amount of time it takes for the actual velocity to converge
+  // to the desired velocity.
+  float _velocitySettleTime = 1.0f;
+  glm::vec3 _velocity{};
+
+  // Each axis will be [-1.0, 0.0, 1.0]. Normalize the vector to maintain
+  // a consistent overall magnitude.
+  glm::vec3 _targetDirection{};
+  float _targetSpeed = 5.0f;
 
   Camera _camera;
 };
--- Src/CameraController.cpp
+++ Src/CameraController.cpp
@@ -14,18 +14,18 @@ CameraController::CameraController(
   // TODO: allow bindings from config file
 
   // Bind inputs. 
-  inputManager.addBinding({GLFW_KEY_W, GLFW_REPEAT, 0}, std::bind(&CameraController::_moveForward, this));
-  inputManager.addBinding({GLFW_KEY_W, GLFW_PRESS, 0}, std::bind(&CameraController::_moveForward, this));
-  inputManager.addBinding({GLFW_KEY_A, GLFW_REPEAT, 0}, std::bind(&CameraController::_strafeLeft, this));
-  inputManager.addBinding({GLFW_KEY_A, GLFW_PRESS, 0}, std::bind(&CameraController::_strafeLeft, this));
-  inputManager.addBinding({GLFW_KEY_S, GLFW_REPEAT, 0}, std::bind(&CameraController::_moveBackward, this));
-  inputManager.addBinding({GLFW_KEY_S, GLFW_PRESS, 0}, std::bind(&CameraController::_moveBackward, this));
-  inputManager.addBinding({GLFW_KEY_D, GLFW_REPEAT, 0}, std::bind(&CameraController::_strafeRight, this));
-  inputManager.addBinding({GLFW_KEY_D, GLFW_PRESS, 0}, std::bind(&CameraController::_strafeRight, this));
-  inputManager.addBinding({GLFW_KEY_Q, GLFW_REPEAT, 0}, std::bind(&CameraController::_descend, this));
-  inputManager.addBinding({GLFW_KEY_Q, GLFW_PRESS, 0}, std::bind(&CameraController::_descend, this));
-  inputManager.addBinding({GLFW_KEY_E, GLFW_REPEAT, 0}, std::bind(&CameraController::_ascend, this));
-  inputManager.addBinding({GLFW_KEY_E, GLFW_PRESS, 0}, std::bind(&CameraController::_ascend, this));
+  inputManager.addBinding({GLFW_KEY_W, GLFW_PRESS, 0}, std::bind(&CameraController::_updateTargetDirection, this, 2, -1));
+  inputManager.addBinding({GLFW_KEY_W, GLFW_RELEASE, 0}, std::bind(&CameraController::_updateTargetDirection, this, 2, 0));
+  inputManager.addBinding({GLFW_KEY_A, GLFW_PRESS, 0}, std::bind(&CameraController::_updateTargetDirection, this, 0, -1));
+  inputManager.addBinding({GLFW_KEY_A, GLFW_RELEASE, 0}, std::bind(&CameraController::_updateTargetDirection, this, 0, 0));
+  inputManager.addBinding({GLFW_KEY_S, GLFW_PRESS, 0}, std::bind(&CameraController::_updateTargetDirection, this, 2, 1));
+  inputManager.addBinding({GLFW_KEY_S, GLFW_RELEASE, 0}, std::bind(&CameraController::_updateTargetDirection, this, 2, 0));
+  inputManager.addBinding({GLFW_KEY_D, GLFW_PRESS, 0}, std::bind(&CameraController::_updateTargetDirection, this, 0, 1));
+  inputManager.addBinding({GLFW_KEY_D, GLFW_RELEASE, 0}, std::bind(&CameraController::_updateTargetDirection, this, 0, 0));
+  inputManager.addBinding({GLFW_KEY_Q, GLFW_PRESS, 0}, std::bind(&CameraController::_updateTargetDirection, this, 1, -1));
+  inputManager.addBinding({GLFW_KEY_Q, GLFW_RELEASE, 0}, std::bind(&CameraController::_updateTargetDirection, this, 1, 0));
+  inputManager.addBinding({GLFW_KEY_E, GLFW_PRESS, 0}, std::bind(&CameraController::_updateTargetDirection, this, 1, 1));
+  inputManager.addBinding({GLFW_KEY_E, GLFW_RELEASE, 0}, std::bind(&CameraController::_updateTargetDirection, this, 1, 0));
 }
 
 CameraController::~CameraController() {
@@ -33,7 +33,31 @@ CameraController::~CameraController() {
 }
 
 void CameraController::tick(float deltaTime) {
+  const glm::mat4& transform = this->_camera.getTransform();
+  glm::vec3 position(transform[3]);
 
+  glm::vec3 targetVelocity;
+  float directionMagnitude = glm::length(this->_targetDirection);
+  if (directionMagnitude < 0.001f) {
+    targetVelocity = glm::vec3(0.0f, 0.0f, 0.0f);
+  } else {
+    glm::vec3 targetVelocityLocal = this->_targetSpeed * glm::normalize(this->_targetDirection);
+    targetVelocity = glm::vec3(transform * glm::vec4(targetVelocityLocal, 0.0f));
+  }
+  
+  // Feedback controller
+  float K = 4.0f / this->_velocitySettleTime;
+  glm::vec3 acceleration = K * (targetVelocity - this->_velocity);
+
+  // Euler integration step
+  this->_velocity += acceleration * deltaTime;
+  glm::vec3 newPosition = position + this->_velocity * deltaTime;
+
+  this->_camera.setPosition(newPosition);
+}
+
+void CameraController::_updateTargetDirection(uint32_t axis, int dir) {
+  this->_targetDirection[axis] = static_cast<float>(dir);
 }
 
 static void moveLocal(Camera& camera, const glm::vec3& localDisplacement) {
@@ -41,30 +65,6 @@ static void moveLocal(Camera& camera, const glm::vec3& localDisplacement) {
 
   glm::vec3 newPos(transform[3] + transform * glm::vec4(localDisplacement, 0.0f));
   camera.setPosition(newPos);
-}
-
-void CameraController::_moveForward() {
-  moveLocal(this->_camera, glm::vec3(0.0f, 0.0f, -this->_movementSpeed));  
-}
-
-void CameraController::_moveBackward() {
-  moveLocal(this->_camera, glm::vec3(0.0f, 0.0f, this->_movementSpeed));    
-}
-
-void CameraController::_strafeLeft() {
-  moveLocal(this->_camera, glm::vec3(-this->_movementSpeed, 0.0f, 0.0f));    
-}
-
-void CameraController::_strafeRight() {
-  moveLocal(this->_camera, glm::vec3(this->_movementSpeed, 0.0f, 0.0f));    
-}
-
-void CameraController::_ascend() {
-  moveLocal(this->_camera, glm::vec3(0.0f, this->_movementSpeed, 0.0f));    
-}
-
-void CameraController::_descend() {
-  moveLocal(this->_camera, glm::vec3(0.0f, -this->_movementSpeed, 0.0f));    
 }
 
 void CameraController::_updateMouse(float x, float y) {
