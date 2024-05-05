74a26fbad947bf5a61887fbdd5d3b9bdf813d5c1
Nithin Pranesh <nithinpra7@gmail.com>
2024-05-05 16:55:24 -0700

    StackVector: resize, erase, eraseSwap

--- Include/Althea/Containers/StackVector.h
+++ Include/Althea/Containers/StackVector.h
@@ -21,12 +21,33 @@ public:
   iterator end() { return {&m_pData[m_count]}; }
   const_iterator end() const { return {&m_pData[m_count]}; }
 
-  void push_back(const _T& e) { new (&m_pData[m_count++]) _T(e); }
+  void push_back(const _T& e) {
+    assert(m_count < m_capacity);
+    new (&m_pData[m_count++]) _T(e);
+  }
 
-  void push_back(_T&& e) { new (&m_pData[m_count++]) _T(std::move(e)); }
+  void push_back(_T&& e) {
+    assert(m_count < m_capacity);
+    new (&m_pData[m_count++]) _T(std::move(e));
+  }
 
   template <typename... Args> void emplace_back(Args... args) {
+    assert(m_count < m_capacity);
     new (&m_pData[m_count++]) _T(std::forward<Args>(args));
+  }
+
+  void resize(size_t count) {
+    if (count < m_count) {
+      for (size_t i = count; i < m_count; ++i)
+        m_pData[i].~_T();
+      m_count = count;
+    } else if (count > m_count) {
+      assert(count <= m_capacity);
+      for (size_t i = m_count; i < count; ++i)
+         new (&m_pData[i]) _T();
+    }
+
+    m_count = count;
   }
 
   void clear() {
@@ -35,7 +56,29 @@ public:
     m_count = 0;
   }
 
-  // TODO: Might want to support removals...
+  void erase(size_t i) {
+    assert(i < m_count);
+    m_pData[i++].~_T();
+    for (; i < m_count; ++i) {
+      new (&m_pData[i-1]) _T(std::move(m_pData[i]));
+      m_pData[i].~_T();
+    }
+    --m_count;
+  }
+
+  // erases element and moves last element into removed slot
+  void eraseSwap(size_t i) {
+    assert(i < m_count);
+    m_pData[i++].~_T();
+    --m_count;
+    if (i < m_count) {
+      new (&m_pData[i]) _T(std::move(m_pData[m_count]));
+      m_pData[m_count].~_T();
+    }
+  }
+
+  size_t size() const { return m_count; }
+  size_t capacity() const { return m_capacity; }
 
 private:
   _T* m_pData;
