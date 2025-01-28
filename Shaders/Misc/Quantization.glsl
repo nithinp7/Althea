b3e64d83ceff15232d4c2f5d40b16f42c8dd96f3
Nithin Pranesh <nithinpra7@gmail.com>
2025-01-28 00:04:14 -0800

    Fix bias in u8 quantization

--- Shaders/Misc/Quantization.glsl
+++ Shaders/Misc/Quantization.glsl
@@ -6,11 +6,11 @@ uint quantizeFloatToU8(float f, float offs, float scale) {
   f -= offs;
   f /= scale;
   f = clamp(f, 0.0, 1.0);
-  return clamp(uint(f * 256.0), 0, 255);
+  return clamp(uint(round(f * 255.0)), 0, 255);
 }
 
 float dequantizeU8ToFloat(uint u, float offs, float scale) {
-  float f = float(u & 0xFF) / 256.0;
+  float f = float(u & 0xFF) / 255.0;
   f *= scale;
   f += offs;
   return f;
