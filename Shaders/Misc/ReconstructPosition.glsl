5049081ac3476645c2f65771fef6d6b2aa96b9d3
Nithin Pranesh <nithinpra7@gmail.com>
2025-02-19 22:32:18 -0800

    ReconstructPosition utility, change function to take in inv-proj and inv-view matrices, instead of assuming global uniforms name

--- Shaders/Misc/ReconstructPosition.glsl
+++ Shaders/Misc/ReconstructPosition.glsl
@@ -1,7 +1,7 @@
 #ifndef _RECONSTRUCTPOSITION_
 #define _RECONSTRUCTPOSITION_
 
-vec3 reconstructPosition(vec2 uv, float dRaw) {
+vec3 reconstructPosition(vec2 uv, float dRaw, mat4 inverseProjection, mat4 inverseView) {
   // TODO: Stop hardcoding this
   float near = 0.01;
   float far = 1000.0;
@@ -10,15 +10,15 @@ vec3 reconstructPosition(vec2 uv, float dRaw) {
   vec2 ndc = 2.0 * uv - vec2(1.0);
 
   vec4 camPlanePos = vec4(ndc, 2.0, 1.0);
-  vec4 dirH = globals.inverseProjection * camPlanePos;
+  vec4 dirH = inverseProjection * camPlanePos;
   dirH.xyz /= dirH.w;
   dirH.w = 0.0;
-  dirH = globals.inverseView * dirH;
+  dirH = inverseView * dirH;
   vec3 dir = normalize(dirH.xyz);
 
-  float f = dot(dir, globals.inverseView[2].xyz);
+  float f = dot(dir, inverseView[2].xyz);
 
-  return globals.inverseView[3].xyz + d * dir / f;
+  return inverseView[3].xyz + d * dir / f;
 }
 
 #endif // _RECONSTRUCTPOSITION_
