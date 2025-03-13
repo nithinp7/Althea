e7d61c53a5f74af3931b916238827738465ba9a8
Nithin Pranesh <nithinpra7@gmail.com>
2025-03-13 16:52:38 -0700

    Fix reconstructLinearDepth

--- Shaders/Misc/ReconstructPosition.glsl
+++ Shaders/Misc/ReconstructPosition.glsl
@@ -21,12 +21,24 @@ vec3 reconstructPosition(vec2 uv, float dRaw, mat4 inverseProjection, mat4 inver
   return inverseView[3].xyz + d * dir / f;
 }
 
-float reconstructLinearDepth(float dRaw) {
-// TODO: Stop hardcoding this
+float reconstructLinearDepth(vec2 uv, float dRaw, mat4 inverseProjection, mat4 inverseView) {
+  // TODO: Stop hardcoding this
   float near = 0.01;
   float far = 1000.0;
   float d = far * near / (dRaw * (far - near) - far);
-  return d;
+
+  vec2 ndc = 2.0 * uv - vec2(1.0);
+
+  vec4 camPlanePos = vec4(ndc, 2.0, 1.0);
+  vec4 dirH = inverseProjection * camPlanePos;
+  dirH.xyz /= dirH.w;
+  dirH.w = 0.0;
+  dirH = inverseView * dirH;
+  vec3 dir = normalize(dirH.xyz);
+
+  float f = dot(dir, inverseView[2].xyz);
+
+  return d / f;
 }
 
 #endif // _RECONSTRUCTPOSITION_
