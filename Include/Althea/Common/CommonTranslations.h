3de7af7b7e23c69af08e9dec9f29222c5174afc6
Nithin Pranesh <nithinpra7@gmail.com>
2025-10-09 21:00:24 -0700

    Hlsl compat in CommonTranslations.h

--- Include/Althea/Common/CommonTranslations.h
+++ Include/Althea/Common/CommonTranslations.h
@@ -11,6 +11,21 @@ using namespace glm;
 #endif
 
 #ifdef IS_SHADER
+
+#ifdef IS_HLSL_SHADER
+#define uvec4 uint4
+#define uvec3 uint3
+#define uvec2 uint2
+#define ivec4 int4
+#define ivec3 int3
+#define ivec2 int2
+#define vec4 float4
+#define vec3 float3
+#define vec2 float2
+#define mat2 float2x2
+#define mat3 float3x3
+#define mat4 float4x4
+#endif // IS_HLSL_SHADER
 // packed formats
 #define u16vec4 uvec2
 uvec4 unpack_u16vec4(uvec2 packed) { 
